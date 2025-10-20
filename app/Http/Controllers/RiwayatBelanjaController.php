0e10adf1568447ac248981590c169e82c5ffaca7
chairil0303 <chairilsyahrain24@gmail.com>
2025-10-20 22:11:33 +0700

    fix index, edit blade belanja

--- app/Http/Controllers/RiwayatBelanjaController.php
+++ app/Http/Controllers/RiwayatBelanjaController.php
@@ -12,7 +12,7 @@ class RiwayatBelanjaController extends Controller
 {
     public function index(Request $request)
     {
-        $riwayatQuery = \App\Models\RiwayatBelanja::with('barang');
+        $riwayatQuery =RiwayatBelanja::with('barang');
 
         // Filter bulan (format: YYYY-MM dari <input type="month">)
         if ($request->filled('bulan')) {
@@ -81,5 +81,63 @@ class RiwayatBelanjaController extends Controller
         return redirect()->route('bengkel.belanja.index')
             ->with('success', 'Belanja barang berhasil disimpan!');
     }
+
+    public function edit($id)
+{
+    $belanja = RiwayatBelanja::with('barang')->findOrFail($id);
+    $barangs = Barang::orderBy('nama_barang')->get();
+    return view('bengkel.belanja.edit', compact('belanja', 'barangs'));
+}
+
+public function update(Request $request, $id)
+{
+    $belanja = RiwayatBelanja::findOrFail($id);
+
+    $request->validate([
+        'barang_id' => 'required|exists:barangs,id',
+        'kuantiti' => 'required|integer|min:1',
+        'harga_beli' => 'required',
+        'harga_jual' => 'required',
+    ]);
+
+    // format angka
+    $hargaBeli = (int) preg_replace('/\D/', '', $request->harga_beli);
+    $hargaJual = (int) preg_replace('/\D/', '', $request->harga_jual);
+
+    $barang = Barang::find($request->barang_id);
+
+    // hitung selisih kuantiti
+    $selisih = $request->kuantiti - $belanja->kuantiti;
+
+    // update stok dan harga
+    $barang->update([
+        'stok' => $barang->stok + $selisih,
+        'harga_beli' => $hargaBeli,
+        'harga_jual' => $hargaJual,
+    ]);
+
+    // update riwayat belanja
+    $belanja->update([
+        'kuantiti' => $request->kuantiti,
+        'harga_beli' => $hargaBeli,
+        'total_belanja' => $hargaBeli * $request->kuantiti,
+    ]);
+
+    return redirect()->route('bengkel.belanja.index')->with('success', 'Riwayat belanja berhasil diperbarui!');
+}
+
+public function destroy($id)
+{
+    $belanja = RiwayatBelanja::findOrFail($id);
+    $barang = Barang::find($belanja->barang_id);
+
+    // kembalikan stok ke semula
+    $barang->stok -= $belanja->kuantiti;
+    $barang->save();
+
+    $belanja->delete();
+
+    return redirect()->route('bengkel.belanja.index')->with('success', 'Riwayat belanja berhasil dihapus!');
+}   
 
 }
